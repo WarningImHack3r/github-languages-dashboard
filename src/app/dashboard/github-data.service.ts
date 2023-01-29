6ce30681e73614566a4a6a440573b37819c49cdc
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2023-01-29 20:37:47 +0100

    Initial GraphQL experiment

--- src/app/dashboard/github-data.service.ts
+++ src/app/dashboard/github-data.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { graphql } from "@octokit/graphql";
+import type { GraphQlQueryResponseData } from "@octokit/graphql";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class GithubDataService {
+  graphqlWithAuth = graphql.defaults({
+    headers: {
+      authorization: `token ${process.env.GITHUB_TOKEN}` // TODO: how to inject this token?
+    }
+  });
 
-  constructor() { }
+  constructor() {}
 
   getGithubData(): Observable<any> {
     const data = {
@@ -16,6 +23,22 @@ export class GithubDataService {
     };
 
     return of(data);
+  }
 
+  async getRepo(owner: string, repo: string): Promise<Observable<any>> {
+    // TODO: how to console.log to test this function?
+    const { repository } = await this.graphqlWithAuth<GraphQlQueryResponseData>({
+      query: `
+        query {
+          repository(owner: $owner, name: $repo) {
+            name
+            url
+          }
+        }
+      `,
+      owner: owner,
+      repo: repo
+    });
+    return of(repository); // TODO: is this the right way to return the data?
   }
 }
